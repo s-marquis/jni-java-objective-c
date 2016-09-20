9e82ab5590cef0098d74598e81ac24748cbf35a9
Samuel Marquis <samuel.marquis@unic.com>
2016-09-20 19:34:11 +0200

    calling obj-c from java

--- calling_c_from_java/build.sh
+++ calling_c_from_java/build.sh
@@ -11,8 +11,16 @@ javac AlgoViz.java
 
 # locate jni.h
 
-gcc -o libwarnapp_algo.dylib -shared -I/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/include/ \
+gcc -o libwarnapp_algo_c.dylib -shared -I/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/include/ \
 	-I/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/include/darwin/ warnapp_algo.c 
+
+
+gcc -framework Foundation \
+	-o libwarnapp_algo.dylib -shared -I/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/include/ \
+	-I/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk/Contents/Home/include/darwin/ warnapp_algo.m
+
+
+
 
 
 java -Djava.library.path=`pwd` AlgoViz
---
+++ calling_c_from_java/warnapp_algo.m
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <stdio.h>
+#import <Foundation/Foundation.h>
+
+JNIEXPORT void JNICALL Java_AlgoViz_runAlgo15
+  (JNIEnv * env, jobject o) {
+
+  	NSString* str = @"computing in objective-c.....\n";
+	NSLog(@"%@", str);
+
+};
+
+
+
+
